224fecf6d4b6f83995238c5d0a2cbc9f103a73ea
Arsen Gadjiusmanov <arsen.gadjiusmanov@mail.ru>
2022-12-06 09:46:00 +0300

    update db

--- database/database.sql
+++ database/database.sql
@@ -1,18 +1,23 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS authors;
+
 CREATE TABLE users (
 	user_id serial PRIMARY KEY,
-	first_name varchar(128),
+	first_name varchar(128) NOT NULL,
 	last_name varchar(128),
 	avatar varchar(128),
 	login varchar(128) NOT NULL,
 	password text NOT NULL,
 	create_at date,
-	admin boolean
+	admin boolean NOT NULL
 );
 
 CREATE TABLE authors (
 	authors_id serial PRIMARY KEY,
 	fk_user_id  varchar(128),
-	descripton varchar(512)
+	descripton varchar(512),
+
+	CONSTRAINT fk_authors_user_id FOREIGN KEY(fk_user_id) REFERENCES users(user_id) NOT NULL
 );
 
 CREATE TABLE categories (
@@ -34,8 +39,12 @@ CREATE TABLE news (
 	tags array,
 	body text,
 	main_img text,
-	other_imgs array
+	other_imgs array,
 	title varchar(512)
+
+	CONSTRAINT fk_news_author_id FOREIGN KEY(fk_author_id) REFERENCES authors(author_id)
+	CONSTRAINT fk_news_category_id FOREIGN KEY(fk_category_id) REFERENCES catagories(catagory_id)
+
 );
 
 CREATE TABLE news_comments (
